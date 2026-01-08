1a9a4e1b8d60a756a2e8c6a18763a434db643fc7
Marlen Neubert <marlen.neubert@kit.edu>
2026-01-08 13:33:07 +0100

    add debug statements regularizers

--- mace/tools/train.py
+++ mace/tools/train.py
@@ -168,6 +168,10 @@ def _method_pca_reg_term(model: torch.nn.Module) -> Optional[torch.Tensor]:
     if not method_pca.requires_grad:
         return None
 
+    # check
+    reg_value = ((method_pca - method_pca_ref) ** 2).mean()
+    print('Regularizer value:', reg_value.item())
+
     return (method_pca - method_pca_ref).pow(2).mean()
 
 
@@ -478,6 +482,7 @@ def take_step(
         loss = loss_fn(pred=output, ref=batch)
         if method_pca_reg_weight > 0.0:
             reg = _method_pca_reg_term(model)
+            print('PCA regularizer term:', reg)
             if reg is not None:
                 loss = loss + method_pca_reg_weight * reg
 
